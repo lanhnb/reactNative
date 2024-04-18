52f4e4818535a261f4a0cc0176403673cfef44f4
lanhnb <lanhnb@gmail.com>
2024-04-18 18:57:04 +0700

    Ngay 18/04

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,77 +1,97 @@
-import { Image, StyleSheet, Text, View, TouchableOpacity, TextInput, ScrollView } from "react-native";
+import {
+  Image,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TextInput,
+  ScrollView,
+} from "react-native";
 import React, { useState } from "react";
 import { Stack } from "expo-router";
 import { Colors } from "@/constants/Colors";
 import { Ionicons } from "@expo/vector-icons";
-import {useHeaderHeight} from '@react-navigation/elements'
+import { useHeaderHeight } from "@react-navigation/elements";
 import { SearchBar } from "react-native-screens";
 import CategoryButtons from "@/components/CategoryButtons";
 import Listings from "@/components/Listings";
 import listingData from "@/data/destinations.json";
-
+import GroupListings from "@/components/GroupListings";
+import groupData from "@/data/groups.json";
 
 const Page = () => {
   const headerHeight = useHeaderHeight();
-  const [category, setCategory] = useState('All')
-  
-  const onCatChanged = (category: string)=>{
-   
+  const [category, setCategory] = useState("All");
+
+  const onCatChanged = (category: string) => {
+    console.log("Category:", category);
     setCategory(category);
-  }
+  };
 
   return (
     <>
-    <Stack.Screen
-      options={{
-        headerTransparent: true,
-        headerTitle: "",
-        headerLeft: () => (
-          <TouchableOpacity onPress={() => {}} style={{ marginLeft: 20 }}>
-            <Image
-              source={{
-                uri: "https://res.cloudinary.com/dxnhv54sl/image/upload/v1695431219/logo/epiu3addc0ing9mk4l2p.png",
+      <Stack.Screen
+        options={{
+          headerTransparent: true,
+          headerTitle: "",
+          headerLeft: () => (
+            <TouchableOpacity onPress={() => {}} style={{ marginLeft: 20 }}>
+              <Image
+                source={{
+                  uri: "https://res.cloudinary.com/dxnhv54sl/image/upload/v1695431219/logo/epiu3addc0ing9mk4l2p.png",
+                }}
+                style={{ width: 40, height: 40, borderRadius: 5 }}
+              />
+            </TouchableOpacity>
+          ),
+          headerRight: () => (
+            <TouchableOpacity
+              onPress={() => {}}
+              style={{
+                marginRight: 20,
+                backgroundColor: Colors.white,
+                padding: 10,
+                borderRadius: 10,
+                shadowColor: "#171717",
+                shadowOffset: { width: 2, height: 4 },
+                shadowOpacity: 0.2,
+                shadowRadius: 3,
               }}
-              style={{ width: 40, height: 40, borderRadius: 5 }}
-            />
-          </TouchableOpacity>
-        ),
-        headerRight: () => (
-          <TouchableOpacity
-            onPress={()=>{}} 
-            style={{
-              marginRight:20,
-              backgroundColor:Colors.white,
-              padding:10,
-              borderRadius:10,
-              shadowColor:'#171717',
-              shadowOffset:{width:2, height:4},
-              shadowOpacity:0.2,
-              shadowRadius:3,
-            }} 
-          >
-            <Ionicons name="notifications" size={20} color={Colors.black}/>
-          </TouchableOpacity>
-        ),
-      }}
-    />
-    <ScrollView>
-    <View style={[styles.container, {paddingTop: headerHeight}]}>
-      <Text style={styles.headingText}>Explore the Beautiful World!</Text>
-      <View style={styles.searchSelectionWrapper}>
-        <View style={styles.searchBar}>
-          <Ionicons name='search' size={18} style={{paddingRight:10, marginTop:6}}/>
-          <TextInput placeholder="Search..." style={{color:Colors.black}}/>
-        </View>
-        <TouchableOpacity onPress={()=>{}} style={styles.filterBtn}>
-          <Ionicons name="options" size={28} style={{color:Colors.white}} />
+            >
+              <Ionicons name="notifications" size={20} color={Colors.black} />
+            </TouchableOpacity>
+          ),
+        }}
+      />
 
-        </TouchableOpacity>
+      <View style={[styles.container, { paddingTop: headerHeight }]}>
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <Text style={styles.headingText}>Explore the Beautiful World!</Text>
+          <View style={styles.searchSelectionWrapper}>
+            <View style={styles.searchBar}>
+              <Ionicons
+                name="search"
+                size={18}
+                style={{ paddingRight: 10, marginTop: 6 }}
+              />
+              <TextInput
+                placeholder="Search..."
+                style={{ color: Colors.black }}
+              />
+            </View>
+            <TouchableOpacity onPress={() => {}} style={styles.filterBtn}>
+              <Ionicons
+                name="options"
+                size={28}
+                style={{ color: Colors.white }}
+              />
+            </TouchableOpacity>
+          </View>
+          <CategoryButtons onCategoryChanged={onCatChanged} />
+          <Listings listings={listingData} category={category} />
+          <GroupListings listings={groupData} />
+        </ScrollView>
       </View>
-      <CategoryButtons onCategoryChanged={onCatChanged}/>
-      <Listings listings={listingData} />
-      
-    </View>
-    </ScrollView>
     </>
   );
 };
@@ -80,34 +100,30 @@ export default Page;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingHorizontal:20,
-    backgroundColor:Colors.bgColor,
+    paddingHorizontal: 20,
+    backgroundColor: Colors.bgColor,
   },
-  headingText:{
+  headingText: {
     fontSize: 28,
-    fontWeight:'800',
-    color:Colors.black,
-    marginTop:10,
-
+    fontWeight: "800",
+    color: Colors.black,
+    marginTop: 10,
   },
-  searchSelectionWrapper:{
-    flexDirection:'row',
-    marginVertical:20,
-
+  searchSelectionWrapper: {
+    flexDirection: "row",
+    marginVertical: 20,
   },
-  searchBar:{
-    flex:1,
-    flexDirection:'row',
-    backgroundColor:Colors.white,
-    padding:16,
-    borderRadius:10,
+  searchBar: {
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: Colors.white,
+    padding: 16,
+    borderRadius: 10,
   },
-  filterBtn:{
-    backgroundColor:'#ff8000',
-    padding:12,
-    borderRadius:10,
-    marginLeft:20,
-    
-
+  filterBtn: {
+    backgroundColor: "#ff8000",
+    padding: 12,
+    borderRadius: 10,
+    marginLeft: 20,
   },
 });

e915ac92f05a6d599144fcd34c2141e7d69a8376
lanhnb <lanhnb@gmail.com>
2024-04-24 06:41:46 +0700

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	app/listing/[id].tsx
    #	components/GroupListings.tsx
    #	package.json

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,20 +1,66 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  TouchableOpacity,
+  Modal,
+} from "react-native";
 import React from "react";
+import MenuRe from "@/components/PopupMenu";
+import { Colors } from "@/constants/Colors";
+import { Icon } from "react-native-paper";
+import PopupMenu from "@/components/PopupMenu";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { Stack, router } from "expo-router";
+import Xkld from "@/components/Xkld";
 
-const Page =()=>{
-    return (
-        <View style={styles.container}>
-            <Text>Search</Text>
-            
-        </View>
-    )
-}
-export default Page
+
+const Page = () => {
+  return (
+    <>
+      <Stack.Screen
+        options={{
+          headerTransparent: true,
+          headerTitle: "Settings",
+         headerStyle:{backgroundColor:'black'},
+          
+          headerLeft: () => (
+            <View>
+              <MaterialCommunityIcons
+                onPress={router.back}
+                name="arrow-left"
+                size={26}
+                color={"#212121"}
+              />
+            </View>
+          ),
+          headerRight: () => (
+            <View>
+              <PopupMenu />
+            </View>
+          ),
+        }}
+      />
+      <View style={styles.xkld}>
+       <Xkld/>
+      </View>
+    </>
+  );
+};
+export default Page;
 
 const styles = StyleSheet.create({
-    container: {
-        flex:1,
-        justifyContent:'center',
-        alignItems:'center',
-    }
-})
+  container: {
+    flex: 1,
+  },
+  title: {},
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  xkld:{
+    top:80,
+
+  }
+});
---
+++ components/PopupMenu.tsx
@@ -0,0 +1,91 @@
+import { Colors } from "@/constants/Colors";
+import { Ionicons } from "@expo/vector-icons";
+import * as React from "react";
+import {
+  View,
+  TouchableOpacity,
+  SafeAreaView,
+  StyleSheet,
+  Modal,
+  Text,
+  Animated,
+  Easing,
+} from "react-native";
+import { Button, Menu, Divider, PaperProvider } from "react-native-paper";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+const PopupMenu = () => {
+  const [visible, setVisible] = React.useState(false);
+  const scale = React.useRef(new Animated.Value(0)).current;
+
+  const openMenu = () => setVisible(true);
+
+  const closeMenu = () => setVisible(false);
+  const options = [
+    { title: "Publicar", icon: "grid", action: () => alert("publicar") },
+    { title: "Story", icon: "plus", action: () => alert("Story") },
+    { title: "Video do Reels", icon: "video", action: () => alert("Reels") },
+    {
+      title: "Transmissao ao vivo",
+      icon: "antenna",
+      action: () => alert("Transmissao"),
+    },
+  ];
+function resizeBox(to){
+  to === 1 && setVisible(true);
+  Animated.timing(scale,{
+    toValue: to,
+    useNativeDriver:true,
+    duration:2000,
+    easing:Easing.linear,
+  }).start(()=> to === 0 && setVisible(false));
+}
+  return (
+    <>
+      <TouchableOpacity onPress={()=>resizeBox(1)}>
+        <MaterialCommunityIcons
+          name="plus-circle-outline"
+          size={26}
+          color={Colors.black}
+        />
+      </TouchableOpacity>
+      <Modal transparent visible={visible}>
+        <SafeAreaView
+          style={{ flex: 1 }}
+          onTouchStart={() => resizeBox(0)}
+        >
+          <Animated.View style={[styles.popup,
+          {opacity:scale.interpolate({inputRange:[0,1], outputRange:[0,1]})},
+            {transform:[{scale}],}]}>
+            {options.map((op, i)=>(
+              <TouchableOpacity style={[styles.option,{borderBottomWidth:i===options.length-1 ? 0:1}]} key={i} onPress={()=>op.action}>
+                <Text>{op.title}</Text>
+                <MaterialCommunityIcons name={op.icon} size={26} color={'#212121'} style={{marginLeft:10}}/>
+              </TouchableOpacity>
+            ))}
+          </Animated.View>
+        </SafeAreaView>
+      </Modal>
+    </>
+  );
+};
+const styles = StyleSheet.create({
+  popup:{
+    borderRadius:8,
+    borderColor:Colors.primaryColor,
+    borderWidth:1,
+    backgroundColor:Colors.white,
+    paddingHorizontal:10,
+    position:'absolute',
+    top:50,
+    right:20,
+  },
+  option:{
+    flexDirection:'row',
+    justifyContent:'space-between',
+    alignItems:'center',
+    paddingVertical:7,
+    borderBottomColor:'#ccc',
+  }
+})
+export default PopupMenu;
